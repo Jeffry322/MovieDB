1d09e263d72b9a15d69910918b91ce65c5df42d2
Jeffry322 <67588481+Jeffry322@users.noreply.github.com>
2023-09-30 17:02:13 +0300

    added-repository

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T> where T : class, IAggregateRoot
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> FirstOrDeffaultAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> ListAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
